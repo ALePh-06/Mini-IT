d3b6af04b933fd79cfe759c5d0e02ee3e5110779
ALePh <alif.aql4429@gmail.com>
2025-05-12 23:56:39 +0800

    so things

--- Alep/schema.sql
+++ Alep/schema.sql
@@ -7,10 +7,20 @@ CREATE TABLE courses (
     code TEXT NOT NULL,
     content TEXT NOT NULL
 );
-
-CREATE TABLE templates (
+DROP TABLE IF EXISTS templates;
+    CREATE TABLE templates (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     field_name TEXT NOT NULL,
     field_order INTEGER NOT NULL
 );
+
+
+drop table if exists users;
+    create table users (
+    id integer primary key autoincrement,
+    username text not null,
+    password text not null
+);
+
+DROP TABLE IF EXISTS posts;
